7a38998f3a3ca051a41b01559441bc1e1215b9e5
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-10-01 20:20:41 +0600

    Add layouts

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,29 @@
+import Menu from "@/components/Menu";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-screen flex">
+      {/* LEFT */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4 bg-red-50">
+        <Link
+          href={"/"}
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src="/logo.png" alt="Logo" width={32} height={32} />
+          <span className="hidden lg:block">BestSchool</span>
+        </Link>
+        <Menu />
+      </div>
+      {/* RIGHT */}
+      <div className="w-[86%] md:w-[92] lg:w-[86%] xl:w-[86%]  bg-red-300">
+        R
+      </div>
+    </div>
+  );
+}
