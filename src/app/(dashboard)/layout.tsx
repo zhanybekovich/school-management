71575c80f64ab150030b784410c9abfba5c1adc2
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-10-02 22:04:31 +0600

    Remove bg-color

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -10,7 +10,7 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4 bg-red-50">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
         <Link
           href={"/"}
           className="flex items-center justify-center lg:justify-start gap-2"
@@ -21,9 +21,7 @@ export default function DashboardLayout({
         <Menu />
       </div>
       {/* RIGHT */}
-      <div className="w-[86%] md:w-[92] lg:w-[86%] xl:w-[86%]  bg-red-300">
-        R
-      </div>
+      <div className="w-[86%] md:w-[92] lg:w-[86%] xl:w-[86%]">R</div>
     </div>
   );
 }
