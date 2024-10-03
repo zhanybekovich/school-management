0a57fab5aa97869d0638516d714ebc7fda60fccc
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-10-03 20:11:46 +0600

    Import navbar in layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -21,7 +22,10 @@ export default function DashboardLayout({
         <Menu />
       </div>
       {/* RIGHT */}
-      <div className="w-[86%] md:w-[92] lg:w-[86%] xl:w-[86%]">R</div>
+      <div className="w-[86%] md:w-[92] lg:w-[86%] xl:w-[86%] bg-[#f7f8fa] overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
     </div>
   );
 }
